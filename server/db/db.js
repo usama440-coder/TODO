6fcb85dcbba66f4c7f6bc62917529ea0c9329219
Usama Shaukat <usamamian440@gmail.com>
2023-07-15 21:22:28 +0500

    Test Docker

--- server/db/db.js
+++ server/db/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
-const connectDB = () => {
+const connectDB = async () => {
   try {
     let connection;
     const mode = process.env.MODE;
-    if (mode === "TEST") {
-      connection = mongoose.connect(process.env.MONGO_URI_TEST);
-    } else {
-      connection = mongoose.connect(process.env.MONGO_URI);
-    }
+    connection = await mongoose.connect(process.env.MONGO_URI_TEST);
+    // if (mode === "TEST") {
+    //   connection = await mongoose.connect(process.env.MONGO_URI_TEST);
+    // } else {
+    //   connection = await mongoose.connect(process.env.MONGO_URI);
+    // }
     console.log("Database connected " + mode);
   } catch (error) {
     console.log(error);
